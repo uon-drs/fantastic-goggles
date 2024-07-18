25f6835ca5a4fbf489799784d22fbff0c3c0239a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-07-18 15:43:38 +0100

    Feature/drf OIDC authenticator (#3)
    * add authentication.py
    
    * Create .gitignore

---
+++ fantastic_goggles/authentication.py
@@ -0,0 +1,82 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.request import HttpRequest
+from rest_framework.exceptions import AuthenticationFailed
+from keycloak import KeycloakOpenID
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from jwcrypto.jwt import JWTExpired
+
+
+class OIDCAuthentication(BaseAuthentication):
+    def authenticate(self, request: HttpRequest) -> tuple[AbstractBaseUser, None]:
+        """Authenticate a user using the OIDC protocol.
+
+        Args:
+            request (HttpRequest): The request to authenticate.
+
+        Returns:
+            tuple[AbstractBaseUser, None]: A tuple with the authenticated user.
+        """
+        token = request.headers.get("Authorization")
+        if token:
+            token = token.removeprefix("Bearer ")
+        else:
+            raise AuthenticationFailed("No Bearer token in request")
+
+        keycloak = KeycloakOpenID(
+            server_url=settings.KEYCLOAK_SERVER,
+            realm_name=settings.KEYCLOAK_REALM,
+            client_id=settings.KEYCLOAK_CLIENT,
+        )
+
+        try:
+            user_info = keycloak.decode_token(token)
+        except JWTExpired:
+            raise AuthenticationFailed("Token expired")
+
+        UserModel = get_user_model()
+
+        try:
+            user = UserModel.objects.get(username=user_info["preferred_username"])
+        except UserModel.DoesNotExist:
+            raise AuthenticationFailed("User does not exist")
+
+        return (user, None)
+
+
+class OIDCAuthenticationAsync(BaseAuthentication):
+    async def authenticate(self, request: HttpRequest) -> tuple[AbstractBaseUser, None]:
+        """Asynchronously authenticate a user using the OIDC protocol.
+
+        Args:
+            request (HttpRequest): The request to authenticate.
+
+        Returns:
+            tuple[AbstractBaseUser, None]: A tuple with the authenticated user.
+        """
+        token = request.headers.get("Authorization")
+        if token:
+            token = token.removeprefix("Bearer ")
+        else:
+            raise AuthenticationFailed("No Bearer token in request")
+
+        keycloak = KeycloakOpenID(
+            server_url=settings.KEYCLOAK_SERVER,
+            realm_name=settings.KEYCLOAK_REALM,
+            client_id=settings.KEYCLOAK_CLIENT,
+        )
+
+        try:
+            user_info = await keycloak.a_decode_token(token)
+        except JWTExpired:
+            raise AuthenticationFailed("Token expired")
+
+        UserModel = get_user_model()
+
+        try:
+            user = await UserModel.objects.aget(username=user_info["preferred_username"])
+        except UserModel.DoesNotExist:
+            raise AuthenticationFailed("User does not exist")
+
+        return (user, None)
