d9a0a24c5cd5030d5f0e0a0bbfaf5b62af06dd5a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-07-19 12:01:55 +0100

    rename sigin view to auth

--- fantastic_goggles/async/urls.py
+++ fantastic_goggles/async/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signin", views.a_sign_in_or_sign_up, name="sign_in_or_sign_up"),
+    path("auth", views.a_sign_in_or_sign_up, name="sign_in_or_sign_up"),
     path("callback", views.a_signin_callback, name="callback"),
 ]
