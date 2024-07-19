2b78dece276be350cae32c64bec9e4093e0c02a3
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-07-19 14:56:05 +0100

    sync refresh endpoint

--- fantastic_goggles/sync/urls.py
+++ fantastic_goggles/sync/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("auth", views.sign_in_or_sign_up, name="sign_in_or_sign_up"),
     path("callback", views.signin_callback, name="callback"),
     path("token", views.get_token, name="get_token"),
+    path("refresh", views.refresh_token, name="refresh"),
 ]
