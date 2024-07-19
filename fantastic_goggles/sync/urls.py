4c841d79b17c3b9257c387c70a83ea5cb88e6188
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-07-19 14:24:12 +0100

    Feature/async stuff (#6)
    * async views version
    
    * urls for those views
    
    * move sync stuff to own package
    
    * rename sigin view to auth

--- fantastic_goggles/sync/urls.py
+++ fantastic_goggles/sync/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signin", views.sign_in_or_sign_up, name="sign_in_or_sign_up"),
+    path("auth", views.sign_in_or_sign_up, name="sign_in_or_sign_up"),
     path("callback", views.signin_callback, name="callback"),
 ]
