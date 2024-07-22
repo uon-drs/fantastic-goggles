3d7d7f87bbe2669325ee3ff188481258536258ff
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-07-22 10:53:40 +0100

    async logout

--- fantastic_goggles/async/urls.py
+++ fantastic_goggles/async/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("callback", views.a_signin_callback, name="callback"),
     path("token", views.a_get_token, name="get_token"),
     path("refresh", views.a_refresh_token, name="refresh"),
+    path("logout", views.a_logout, name="logout"),
 ]
